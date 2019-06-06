2450acf19dd26c95e41af64e8a727f2dae2ac76f
Martin Gaiser <martin-gaiser@t-online.de>
2019-06-06 22:17:11 +0200

    now using getter functions

--- commons/commons.go
+++ commons/commons.go
@@ -5,18 +5,21 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-const (
-	// nolint:gochecknoglobals
-	userService = "go.micro.srv.userservice"
-	// nolint:gochecknoglobals
-	hallService = "go.micro.srv.hallservice"
-	// nolint:gochecknoglobals
-	reservationService = "go.micro.srv.reservationservice"
-	// nolint:gochecknoglobals
-	movieService = "go.micro.srv.movieservice"
-	// nolint:gochecknoglobals
-	showService = "go.micro.srv.showservice"
-)
+func GetUserServiceName() string {
+	return "go.micro.srv.userservice"
+}
+func GetHallServiceName() string {
+	return "go.micro.srv.userservice"
+}
+func GetShowServiceName() string {
+	return "go.micro.srv.userservice"
+}
+func GetMovieServiceName() string {
+	return "go.micro.srv.userservice"
+}
+func GetReservationServiceName() string {
+	return "go.micro.srv.userservice"
+}
 
 func CheckId(id int64, idName string) error {
 	if id <= 0 {
